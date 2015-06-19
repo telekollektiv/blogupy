db803c2ad1c1ee6404b12cfd0f6c5c99a5df8ef1
sliker-one <sliker@radic.it>
2015-06-19 04:42:08 +0200

    beginning the "de-germanizing" and removing the atopie context/content

--- forms.py
+++ forms.py
@@ -6,15 +6,15 @@ from wtforms import (TextField, TextAreaField, DateTimeField, SubmitField,
 
 # Mail contact form
 class ContactForm(Form):
-    name = TextField("Dein Name", [validators.Required("Bitte trage Deinen Namen ein")])
-    email = TextField("Deine E-Mail Adresse", [
-                      validators.Required("Bitte trage eine E-Mail Adresse ein!"),
-                      validators.Email("Die E-Mail Adresse ist nicht gültig!")])
-    subject = TextField("Betreff", [
-                        validators.Required("Bitte gib einen Betreff ein!.")])
-    message = TextAreaField("Nachricht", [
-                            validators.Required("Bitte trage eine Nachricht ein!")])
-    submit = SubmitField("Senden")
+    name = TextField("Your name:", [validators.Required("Name required!")])
+    email = TextField("Your mail address", [
+                      validators.Required("Valid mail address required!!"),
+                      validators.Email("Not a valid mail address!")])
+    subject = TextField("Subject:", [
+                        validators.Required("Pleas enter a subject!")])
+    message = TextAreaField("Message", [
+                            validators.Required("Please enter a message!")])
+    submit = SubmitField("Submit")
 
 
 # äöü ÄÖÜ
@@ -22,16 +22,16 @@ class ContactForm(Form):
 # Contribute Form
 class BlogContributeForm(Form):
     author = TextField("Author")
-    title = TextField("Titel", [validators.Required("Bitte gib einen Titel an!")])
-    article = TextAreaField("Artikel", [validators.Required("Bitte schreibe einen Artikel")])
-    submit = SubmitField("Senden")
+    title = TextField("Title", [validators.Required("Please enter a title!")])
+    article = TextAreaField("Article", [validators.Required("Please write an article!")])
+    submit = SubmitField("Submit")
 
 
 class EventContributeForm(Form):
-    title = TextField('Titel', [validators.Required('required')])
-    author = TextField('Wer')
-    location = TextField('Wo', [validators.Required('required')])
-    date = DateTimeField('Wann', [validators.Required('required')], format='%Y-%m-%d %H:%M')
-    stop = DateTimeField('Bis', [validators.Required('required')], format='%Y-%m-%d %H:%M')
-    description = TextAreaField('Beschreibung', [validators.Required('required')])
-    submit = SubmitField('Senden')
+    title = TextField('Event name', [validators.Required('required')])
+    author = TextField('Who')
+    location = TextField('Where', [validators.Required('required')])
+    date = DateTimeField('From', [validators.Required('required')], format='%Y-%m-%d %H:%M')
+    stop = DateTimeField('To', [validators.Required('required')], format='%Y-%m-%d %H:%M')
+    description = TextAreaField('Description', [validators.Required('required')])
+    submit = SubmitField('Submit')
